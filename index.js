3f262cc1587c7e0ff5ef4d5a2da8da3ef86e8644
Jonathan Vargas <jonathanv3232@gmail.com>
2018-03-10 23:30:11 -0500

    implements concatAll

--- index.js
+++ index.js
@@ -27,43 +27,37 @@ Array.prototype.filter = function(predicateFunction) {
 
 //concatAll
 // JSON.stringify([ [1,2,3], [4,5,6], [7,8,9] ].concatAll()) === "[1,2,3,4,5,6,7,8,9]"
-// [1,2,3].concatAll(); // throws an error because this is a one-dimensional array
 
-Array.prototype.filter = function() {
+Array.prototype.concatAll = function() {
   const newArr = [];
   for (let i = 0; i < this.length; i++) {
-    //check if this is array if it is loop throught it and add the elements to the array
-    // if (this[i]) {
-    //   for (let j = 0; j < this.length; j++) {}
-    // }
+    //using apply to spread the arguments to push
+    newArr.push.apply({}, this[i]);
   }
 };
-
-
 
 // [1,2,3].reduce(function(accumulatedValue, currentValue) { return accumulatedValue + currentValue; }); === [6];
 // [1,2,3].reduce(function(accumulatedValue, currentValue) { return accumulatedValue + currentValue; }, 10); === [16];
 Array.prototype.reduce = function(combinerFunc, initialValue) {
   let accumulatedValue, i;
   //if the array is empty then return it
-  if(this.length == 0 ) {
+  if (this.length == 0) {
     return this;
   } else {
-    if(arguments.length == 1) {
+    if (arguments.length == 1) {
       //if there is no initial value supplied then use the first value and skip the first element in the loop
       i = 1;
       accumulatedValue = this[0];
-    } else if(arguments.length >= 2){
-      // if there is an initial value supplied use that 
-       i = 0; 
-       accumulatedValue = initialValue;
+    } else if (arguments.length >= 2) {
+      // if there is an initial value supplied use that
+      i = 0;
+      accumulatedValue = initialValue;
     }
-    
-    for(; i < this.length; i++) {
+
+    for (; i < this.length; i++) {
       accumulatedValue = combinerFunc(accumulatedValue, this[i]);
     }
-    
+
     return [accumulatedValue];
   }
-  
-}
+};
